751568504c57e76668a3cc46ef92cf50787d4ff0
Sean Coates <sean.coates+fooyay@gmail.com>
2022-11-10 11:37:43 -0500

    add goerli configuration

--- truffle.js
+++ truffle.js
@@ -1,3 +1,6 @@
+const HDWalletProvider = require("truffle-hdwallet-provider");
+require('dotenv').config()
+
 module.exports = {
      // See <http://truffleframework.com/docs/advanced/configuration>
      // to customize your Truffle configuration!
@@ -12,6 +15,15 @@ module.exports = {
             port: 8545,
             network_id: "4224",
             gas: 4700000
+          },
+          goerli: {
+            provider: function() {
+              return new HDWalletProvider(process.env.MNEMONIC,
+                "https://goerli.infura.io/v3/" + process.env.INFURA_API_KEY);
+            },
+            network_id: 5,
+            gas: 4500000,
+            gasPrice: 100000000
           }
      }
 };
