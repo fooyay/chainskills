b05cb28ae192a897d95efd73d6f2e4a5dfa48677
Sean Coates <sean.coates+fooyay@gmail.com>
2022-11-11 12:03:41 -0500

    update contracts for solc 0.5 and truffle 5

--- contracts/ChainList.sol
+++ contracts/ChainList.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.18;
+pragma solidity >0.4.99 <0.6.0;
 
 import "./Ownable.sol";
 
@@ -6,7 +6,7 @@ contract ChainList is Ownable {
   // custom types
   struct Article {
     uint id;
-    address seller;
+    address payable seller;
     address buyer;
     string name;
     string description;
@@ -38,7 +38,7 @@ contract ChainList is Ownable {
   }
 
   // sell an article
-  function sellArticle(string _name, string _description, uint256 _price) public {
+  function sellArticle(string memory _name, string memory _description, uint256 _price) public {
     // a new article
     articleCounter++;
 
@@ -46,13 +46,13 @@ contract ChainList is Ownable {
     articles[articleCounter] = Article(
       articleCounter,
       msg.sender,
-      0x0,
+      address(0),
       _name,
       _description,
       _price
     );
 
-    LogSellArticle(articleCounter, msg.sender, _name, _price);
+    emit LogSellArticle(articleCounter, msg.sender, _name, _price);
   }
 
   // fetch the number of articles in the contract
@@ -61,7 +61,7 @@ contract ChainList is Ownable {
   }
 
   // fetch and return all article IDs for articles still for sale
-  function getArticlesForSale() public view returns (uint[]) {
+  function getArticlesForSale() public view returns (uint[] memory) {
     // prepare output array
     uint[] memory articleIds = new uint[](articleCounter);
 
@@ -70,7 +70,7 @@ contract ChainList is Ownable {
     // iterate over articles
     for(uint i = 1; i <= articleCounter; i++) {
       // keep the ID if the article is still for sale
-      if(articles[i].buyer == 0x0) {
+      if(articles[i].buyer == address(0)) {
         articleIds[numberOfArticlesForSale] = articles[i].id;
         numberOfArticlesForSale++;
       }
@@ -85,24 +85,24 @@ contract ChainList is Ownable {
   }
 
   // buy an article
-  function buyArticle(uint _id) payable public {
+  function buyArticle(uint _id) public payable {
     // we check whether there is an article for sale
-    require(articleCounter > 0);
+    require(articleCounter > 0, "There should be at least one article");
 
     // we check that the article exists
-    require(_id > 0 && _id <= articleCounter);
+    require(_id > 0 && _id <= articleCounter, "Article with this id does not exist");
 
     // we retrieve the article
     Article storage article = articles[_id];
 
     // we check that the article has not been sold yet
-    require(article.buyer == 0x0);
+    require(article.buyer == address(0), "Article was already sold");
 
     // we don't allow the seller to buy his own article
-    require(msg.sender != article.seller);
+    require(msg.sender != article.seller, "Seller cannot buy his own article");
 
     // we check that the value sent corresponds to the price of teh article
-    require(msg.value == article.price);
+    require(msg.value == article.price, "Value provided does not match price of article");
 
     // keep buyer's information
     article.buyer = msg.sender;
@@ -111,6 +111,6 @@ contract ChainList is Ownable {
     article.seller.transfer(msg.value);
 
     // trigger the event
-    LogBuyArticle(_id, article.seller, article.buyer, article.name, article.price);
+    emit LogBuyArticle(_id, article.seller, article.buyer, article.name, article.price);
   }
 }
--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,6 @@
+const HDWalletProvider = require("truffle-hdwallet-provider");
+require('dotenv').config()
+
 module.exports = {
      // See <http://truffleframework.com/docs/advanced/configuration>
      // to customize your Truffle configuration!
@@ -5,7 +8,33 @@ module.exports = {
           ganache: {
                host: "localhost",
                port: 7545,
-               network_id: "*" // Match any network id
+               network_id: "*", // Match any network id
+               gas: 47000000
+          },
+          chainskills: {
+            host: "localhost",
+            port: 8545,
+            network_id: "4224",
+            gas: 4700000
+          },
+          goerli: {
+            provider: function() {
+              return new HDWalletProvider(process.env.MNEMONIC,
+                "https://goerli.infura.io/v3/" + process.env.INFURA_API_KEY);
+            },
+            network_id: 5,
+            gas: 4500000,
+            gasPrice: 100000000
           }
+     },
+     compilers: {
+      solc: {
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200
+          }
+        }
+      }
      }
 };
